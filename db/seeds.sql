69fdb75a9e6c025f30b4267a8757de658c1dfe93
Lexie_Murphy <Lexie.Murphy88@gmail.com>
2019-03-10 23:51:30 -0500

    inital commit of files and database finished

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwest Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Pepperjack Burger', true);
