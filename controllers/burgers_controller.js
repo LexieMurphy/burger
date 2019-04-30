24ae781540034e2f1aa97f2328baa27dd8b95cd9
Lexie_Murphy <Lexie.Murphy88@gmail.com>
2019-04-30 00:22:56 -0500

    fixed server.js file + other items

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,7 @@
 var express = require("express");
-
 var router = express.Router();
-
 var burger = require("../models/burger.js");
+
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
