5e3f2bda04b861bbcfb57722058470851de2ff07
Lexie_Murphy <Lexie.Murphy88@gmail.com>
2019-04-30 23:10:56 -0500

    added files/css and other

--- config/orm.js
+++ config/orm.js
@@ -12,26 +12,17 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
-
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
-
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
@@ -66,7 +57,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -86,5 +77,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+
 module.exports = orm;
